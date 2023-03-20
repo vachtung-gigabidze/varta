90068eab0bc86253489bbdd4442e66a6284ea59d
vachtung-gigabidze <dmitry.zyadik@gmail.com>
2023-03-20 23:30:12 +0500

    firebase login

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,15 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:varta/di/injector.dart';
 import 'package:varta/domain/repositories/auth_repository.dart';
+import 'package:varta/firebase_options.dart';
 import 'package:varta/presentation/app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  //await Firebase.initializeApp();
   initInjector();
   await i.get<AuthRepositoryInt>().signInAnonymously();
   runApp(const VideoStreamingApp());
